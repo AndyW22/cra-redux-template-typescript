968c9dfef883d6e707ac5dffaa89fb67be991847
AndyW22 <andrew.waller24@outlook.com>
2021-02-17 16:44:15 +0000

    Initial commit

---
+++ src/app/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+import counterReducer from '../features/counter/counterSlice';
+import logger from 'redux-logger';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export type RootState = ReturnType<typeof store.getState>;
